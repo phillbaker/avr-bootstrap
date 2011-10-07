0ad082fd1a03213ed9c72fbd10c8a799555ba9a4
pbaker <me@retrodict.com>
2011-10-07 18:42:50 -0400

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/* Name: main.c
+ * Author: <insert your name here>
+ * Copyright: <insert your copyright message here>
+ * License: <insert your license reference here>
+ */
+
+#include <avr/io.h>
+
+int main(void)
+{
+		DDRE |= 1<<6;
+		PORTE &= !(1<<6);
+    /* insert your hardware initialization here */
+    for(;;){
+        /* insert your main loop code here */
+    }
+    return 0;   /* never reached */
+}
