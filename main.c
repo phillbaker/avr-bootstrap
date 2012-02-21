3b8b70484a3bd92b67a781f268c7578351665b8c
pbaker <me@retrodict.com>
2012-02-21 15:40:03 -0500

    Do calculation for usart freq.

--- main.c
+++ main.c
@@ -13,13 +13,23 @@
 #include "m_general.h"
 #include "m_usb.h"
 
-void usart_init(unsigned int baud);
+#define TAG_LENGTH 10      //Max length of RFID tag
+#define START_BYTE 0x29 
+#define STOP_BYTE 0x39
+#define BAUD 9600
+#define UBRR_VAL ((F_CPU / (16UL * BAUD)) - 1) //use F_CPU defined at command line of compile flags
+
+#define ITERATION_LENGTH 2000 //time, in ms, given to the user to move hand away
+
+void usart_rx_init(void);
 unsigned char usart_receive(void);
+
+char tag[TAG_LENGTH + 1];//do length + checksum bit
 
 int main(void) {
   m_clockdivide(0); // 16 MHz
   m_usb_init();
-  usart_init(9600);
+  usart_rx_init();
   //D2 	RXD1 	USART receive
   clear(DDRD, 2);
   //D3 	USART transmit
@@ -30,49 +40,65 @@ int main(void) {
   
   unsigned char c;
   while(true) {
-    int i = 0;
-    char buff[3];
-    while(i < 2) {
-      c = usart_receive();
-      buff[i] = c;
-      i++;
-    }
+    // int tag_bytes_read = 0;
+    c = usart_receive();
+    // //wait for start byte
+    // if(c != START_BYTE) {
+    //   continue;
+    // }
+    // m_red(ON);
+    // while(tag_bytes_read < TAG_LENGTH + 1) {
+    //   c = usart_receive();
+    //   if(c == STOP_BYTE)
+    //     break;
+    //   tag[tag_bytes_read] = c;
+    //   tag_bytes_read++;
+    // }
+    
     if(m_usb_isconnected()) {
       //each card consists of 13 bits: start bit, 10 data bits, checksum, end flag; the 10 data bits are pairs of hex numbers
       //take 13 bits out of 24
-      int data[13];
-      data[0] = buff[0] >> 2;
       
-      m_usb_tx_int(data[0]);
-      m_usb_tx_string(" ");
-      
-      m_usb_tx_char(buff[0]);
-      m_usb_tx_char(buff[1]);
-      m_usb_tx_char(buff[2]);
+      // m_usb_tx_int(data[0]);
+      // m_usb_tx_string(" ");
+      // 
+      // m_usb_tx_char(buff[0]);
+      // m_usb_tx_char(buff[1]);
+      // m_usb_tx_char(buff[2]);
       
       //char str[1];
       //sprintf(str, "%c", c);
-      //m_usb_tx_int((int)c);
+      m_usb_tx_int((int)c);
+      // int i;
+      // for(i = 0; i < TAG_LENGTH; i++) {
+      //   m_usb_tx_int((int)tag[i]);
+      //   m_usb_tx_string(" ");
+      // }
       //m_usb_tx_string(&str);
     
       m_usb_tx_string("\n");
     }
-    i = 0;
+    // tag_bytes_read = 0;
   }
 }
 
-void usart_init(unsigned int baud) {
+void usart_rx_init(void) {
   //9600bps, No parity ,8 databits, 1 stopbit (assumed no flow control)
   
   /* Set baud rate */ 
-  UBRR1H = (unsigned char)(baud>>8);
-  UBRR1L = (unsigned char)baud;
+  UBRR1H = (unsigned char)(UBRR_VAL>>8);
+  UBRR1L = (unsigned char)UBRR_VAL;
   /* Enable receiver and transmitter */
   UCSR1B = (1<<RXEN1)|(1<<TXEN1);
-  /* Set frame format: 8 data, 1stop bit */
-  UCSR1C = (0<<USBS1)|(3<<UCSZ10);
+  /* Set frame format: 8 data, 1stop bit (stop bits ignored in reading) */
+  //UCSR1C = (0<<USBS1)|(3<<UCSZ10);
+  // Use 8-bit characters
+  UCSR1C |= (1<<UCSZ10) | (1<<UCSZ11);
 }
 
+/**
+* Blocking function that returns char value in buffer.
+*/
 unsigned char usart_receive(void) {
   //Wait for data to be received
   while (!(UCSR1A & (1<<RXC1)))
