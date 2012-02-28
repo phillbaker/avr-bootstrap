ea103776e071e91ee0442b53900bbf40639a8a2a
pbaker <me@retrodict.com>
2012-02-28 10:30:26 -0500

    Clean up main file and move stuff to external files

--- m_rfid.h
+++ m_rfid.h
@@ -9,10 +9,14 @@
  * TODO: interupt driven code, non-blocking check for bytes available in the queue
  */
 
+#include "m_general.h"
+
 #define START_BYTE 0x2 
 #define STOP_BYTE 0x3
 #define BAUD 9600
 #define UBRR_VAL ((F_CPU / (16UL * BAUD)) - 1) //use F_CPU defined at command line of compile flags
+
+#define TAG_LENGTH 10 //Max length of RFID tag
 
 ///////////////////////////////////////////////////////////////////////////////
 ////Public functions
@@ -21,7 +25,34 @@
 * Init the USART connection: 9600bps, No parity ,8 databits, 1 stopbit (assumed no flow control)
 **/
 void usart_rx_init(void);
+
+/**
+* Blocking function that writes to the char* the value of a RFID tag. Call to block until a RFID tag is read.
+* 
+* @param char* buffer - Should be length + checksum bit. E.g. char tag[TAG_LENGTH + 1].
+* @return int success
+**/
+int m_rfid_bread_tag(char* buffer);
+
+/**
+* Function that that writes to the char* the value of a RFID tag.
+* 
+* @param char* buffer - Should be length + checksum bit. E.g. char tag[TAG_LENGTH + 1].
+* @return int success
+**/
+int m_rfid_read_tag(char* buffer);
+
+///////////////////////////////////////////////////////////////////////////////
+////Internal functions
+///////////////////////////////////////////////////////////////////////////////
+
 /**
 * Blocking function that returns char value in buffer. (Ie blocking receive)
 **/
-unsigned char usart_breceive(void);
+unsigned char m_rfid_bread_byte(void);
+
+/**
+* Read the bytes of the tag, the checksum and the stop byte.
+*/
+int m_rfid_get_tag_bytes(char* buffer);
+
